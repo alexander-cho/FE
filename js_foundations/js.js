b1ff39f111dc5df0bc514f674e11e4bcf0679b2e
Alex Cho <alex.jhcho@gmail.com>
2024-02-12 21:42:03 -0800

    fxns in js

---
+++ js_foundations/js.js
@@ -0,0 +1,12 @@
+let userName = "Alex";
+
+function showMessage() {
+    let userName = "Chris";
+    let message = "Hello there, " + userName;
+    console.log(message);
+}
+
+showMessage();
+console.log(userName);
+
+
